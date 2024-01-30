f1d91c60a55721f5e69874b88c7dbe0bdaa3aa5d
omarelkeshen <oelkeshen5@gmail.com>
2024-01-30 23:48:19 +0200

    set Map Position with GeoLocation

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -4,23 +4,33 @@ import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 're
 import styles from './Map.module.css';
 import PropTypes from "prop-types";
 import { useEffect, useState } from 'react';
+import { useGeolocation } from '../hooks/GoLocation';
+import Button from './Button';
 
 function Map() {
   
   const { cities } = useCitiesContext()
-
   const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const [searchParams] = useSearchParams();
+  const {isLoading: isLoadingPosition, position: geoLocationPosition, getPosition} = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
   useEffect(function() {
     if (mapLat && mapLng)  setMapPosition([mapLat, mapLng]);
-  },[mapLat, mapLng])
+  },[mapLat, mapLng]);
+
+  useEffect(function() {
+    if(geoLocationPosition) setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng])
+  },
+  [geoLocationPosition])
 
   return (
     <div className={styles.mapContainer} >
+      <Button type='position' onClick={getPosition}>
+        {isLoadingPosition? 'Loading...' : "Use your position"}
+      </Button>
       <MapContainer 
         center={mapPosition}
         zoom={6} scrollWheelZoom={true} className={styles.map}>
